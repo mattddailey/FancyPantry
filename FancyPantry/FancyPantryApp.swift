19a1b30cdfcad24f9410f82e6dd7cd59b1e16dc4
matt <matthew_dailey@comcast.com>
2022-07-27 20:36:38 -0400

    Initial project files

---
+++ FancyPantry/FancyPantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FancyPantryApp.swift
+//  FancyPantry
+//
+//  Created by Matt Dailey on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct FancyPantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
